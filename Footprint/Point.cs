048c78783822636a743cc6a3c569d0a9607b385c
AlexSJC <cn_sjc@outlook.com>
2024-05-08 21:07:17 +0800

    update

---
+++ Footprint/Point.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+
+namespace Footprint
+{
+    public class Point
+    {
+        [PrimaryKey]
+        public long Time { get; set; }
+
+        public long Duration { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public float Accuracy { get; set; }
+
+        public double Altitude { get; set; }
+
+        public float Bearing { get; set; }
+
+        public float Speed { get; set; }
+    }
+}
